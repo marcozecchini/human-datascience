a1b162ae7899ad66e8d3312e642bedd2411af365
marcozecchini <marcozecchini.2594@gmail.com>
2020-05-09 10:28:24 +0200

    prova

--- user_ui/client/vue.config.js
+++ user_ui/client/vue.config.js
@@ -1,5 +1,8 @@
 module.exports = {
     publicPath: process.env.NODE_ENV === 'production'
       ? '/human-datascience/'
-      : '/'
+      : '/',
+      devServer: {
+        proxy: `${process.env.VUE_APP_PYTHON_BACKEND}`
+      },
   }
